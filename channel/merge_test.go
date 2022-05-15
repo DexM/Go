cdb3fb270233d6675af05d60fd5bfeca65896136
Kiril Piroženko <kiril@pirozenko.com>
2022-05-15 21:45:12 +0300

    Reproduce memory leak in Merge function

--- channel/merge_test.go
+++ channel/merge_test.go
@@ -3,6 +3,7 @@ package channel_test
 import (
 	"context"
 	"fmt"
+	"runtime"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -71,5 +72,49 @@ func TestMergeReadsChannelsConcurrently(t *testing.T) {
 
 	if diff := cmp.Diff(expected, actual); diff != "" {
 		t.Error(diff)
+	}
+}
+
+func TestMergeDoesNotLeakGoroutines(t *testing.T) {
+	const dataSize = 1024 * 1024
+	var memStatsBefore, memStatsAfter runtime.MemStats
+	var memUsageDiff uint64
+
+	// Disable parallelism for more predictable results
+	oldGoMaxProcs := runtime.GOMAXPROCS(1)
+	defer runtime.GOMAXPROCS(oldGoMaxProcs)
+
+	// Take initial memory reading (for some reason running GC twice gives more stable resutls)
+	runtime.GC()
+	runtime.GC()
+	runtime.ReadMemStats(&memStatsBefore)
+
+	func() {
+		ctx, cancel := context.WithCancel(context.TODO())
+		defer cancel()
+
+		chInput := make(chan []byte, 2)
+		chInput <- make([]byte, dataSize)
+		chInput <- make([]byte, dataSize)
+		close(chInput)
+
+		chRes := channel.Merge(ctx, chInput)
+		<-chRes
+		// Do not drain channel fully, leave 2nd message in the channel
+	}()
+
+	// Take final memory reading (for some reason running GC twice gives more stable resutls)
+	runtime.GC()
+	runtime.GC()
+	runtime.ReadMemStats(&memStatsAfter)
+
+	if memStatsBefore.HeapInuse > memStatsAfter.HeapInuse {
+		memUsageDiff = memStatsBefore.HeapInuse - memStatsAfter.HeapInuse
+	} else {
+		memUsageDiff = memStatsAfter.HeapInuse - memStatsBefore.HeapInuse
+	}
+
+	if memUsageDiff != 0 {
+		t.Errorf("Memory usage diff is greater than 0: %d", memUsageDiff)
 	}
 }
