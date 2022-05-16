b92b3393e927af6402f8911b45baa7b168110b67
Kiril Piroženko <kiril@pirozenko.com>
2022-05-17 01:10:49 +0300

    Proper implementation of async.Execute function

--- async/execute.go
+++ async/execute.go
@@ -1,5 +1,27 @@
 package async
 
+type executeChMessageType[T any] struct {
+	res T
+	err error
+}
+
 func Execute[T any](f func() (T, error)) func() (T, error) {
-	return f
+	ch := make(chan executeChMessageType[T], 1)
+
+	go func() {
+		defer close(ch)
+
+		res, err := f()
+		if err != nil {
+			ch <- executeChMessageType[T]{err: err}
+			return
+		}
+
+		ch <- executeChMessageType[T]{res: res}
+	}()
+
+	return func() (T, error) {
+		msg := <-ch
+		return msg.res, msg.err
+	}
 }
--- async/execute_test.go
+++ async/execute_test.go
@@ -1,7 +1,10 @@
 package async_test
 
 import (
+	"errors"
 	"fmt"
+	"sync"
+	"testing"
 
 	"dexm.lol/async"
 )
@@ -18,4 +21,55 @@ func ExampleExecute() {
 	// Output:
 	// Result: string result of some lengthy operation
 	// Error: <nil>
+}
+
+func TestExecutePassesResultToPromise(t *testing.T) {
+	promise := async.Execute(func() (string, error) {
+		return "dummy result", nil
+	})
+
+	res, err := promise()
+	if err != nil {
+		t.Errorf("Unexpected error received from the promise: %s", err.Error())
+	}
+	if res != "dummy result" {
+		t.Errorf("Unexpected result received from the promise: %s", res)
+	}
+}
+
+func TestExecutePassesErrorToPromise(t *testing.T) {
+	promise := async.Execute(func() (interface{}, error) {
+		return nil, errors.New("dummy error")
+	})
+
+	res, err := promise()
+	if err == nil {
+		t.Error("Expected to receive error from the promise")
+	}
+	if err.Error() != "dummy error" {
+		t.Errorf("Unexpected error received from the promise: %s", err.Error())
+	}
+	if res != nil {
+		t.Errorf("Unexpected result received from the promise: %#v", res)
+	}
+}
+
+func TestExecuteLaunchesAsync(t *testing.T) {
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	promise := async.Execute(func() (string, error) {
+		defer wg.Done()
+		return "dummy result", nil
+	})
+
+	wg.Wait()
+
+	res, err := promise()
+	if err != nil {
+		t.Errorf("Unexpected error received from the promise: %s", err.Error())
+	}
+	if res != "dummy result" {
+		t.Errorf("Unexpected result received from the promise: %s", res)
+	}
 }
