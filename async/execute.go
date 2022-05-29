d2181796018affd5ef883e5112949db84cfc3211
Kiril Piroženko <kiril@pirozenko.com>
2022-05-29 18:52:28 +0300

    Promise type added

--- async/execute.go
+++ async/execute.go
@@ -1,5 +1,9 @@
 package async
 
+// Promise is a function returned by async.Execute function.
+// It can be called later to retrieve result of asynchronous execution.
+type Promise[T any] func() (T, error)
+
 type executeChMessageType[T any] struct {
 	res T
 	err error
@@ -9,7 +13,7 @@ type executeChMessageType[T any] struct {
 // Returns promise which can be called to retrieve function's f result.
 // Calling promise will block execution until function f returns.
 // It is advisable to use context to cancel function's f execution (see example code).
-func Execute[T any](f func() (T, error)) func() (T, error) {
+func Execute[T any](f func() (T, error)) Promise[T] {
 	// This channel is buffered. It will be written to only once.
 	// That way when function f completes, goroutine will end as well (even if promise is never called and channel not drained).
 	ch := make(chan executeChMessageType[T], 1)
